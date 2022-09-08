15eb79651abdd152c4d8d9ca8d8fcf37756ead1b
Victor <vitcheffvi@cy-tech.fr>
2022-09-08 11:18:28 +0200

    add gitignore

--- django_tutorial/django_tutorial/settings.py
+++ django_tutorial/django_tutorial/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+#Import the .env file to secure the BDD
+import environ
+
+# Initialise environment 
+variablesenv = environ.Env()
+environ.Env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
